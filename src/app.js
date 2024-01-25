182012ae17afac80575b3cc417b2918c73215f5f
KevOneRedOne <kevinalves@hotmail.fr>
2024-01-25 15:26:43 +0100

    Update CI/CD

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const options = require('./swaggerOptions.js');
 
-// on utilise cors pour autoriser les requêtes provenant d'autres domaines
 app.use(cors())
 app.options(process.env.FRONTEND_URL, cors());
 
@@ -20,7 +19,6 @@ app.use(swaggerUi.serve);
 
 app.get("/api-docs",swaggerUi.setup(specs));
 
-// on se connecte à la base de données avec la configuration de sequelize
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     port: config.port,
     host: config.host,
@@ -29,13 +27,6 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
         connectTimeout: 60000
     }
 });
-
-// on synchronise sequelize avec la base de données
-// La synchronisation permet de créer les tables dans la base de données si elles n'existent pas
-
-// redisClient.connect().then(() => {
-//     console.log("redis conencted")
-// }).catch(err => console.log(err));
 
 sequelize.sync()
     .then(() => {
@@ -46,17 +37,14 @@ sequelize.sync()
     });
 
     
-    // On définit une route initiale pour vérifier que le serveur fonctionne
     app.get("/", (req, res) => {
         res.send("Welcome to my API");
     })
 
 app.get('/api/data-replication', async (req, res) => {
     try {
-        // je vais chercher mes données en BDD
         const articles = await db.Article.findAll();
         console.log(articles);
-        // je publie mes données dans redis dans le channel articles
         const publishMessage = await redisClient.publish('articles', JSON.stringify(articles));
         console.log(publishMessage);
         res.status(200).json({
@@ -70,10 +58,8 @@ app.get('/api/data-replication', async (req, res) => {
     }
 })
 
-// on définit la route pour récupérer tous les articles avec les controllers
 app.use("/api", router);
 
-// on lance le serveur sur le port défini dans le fichier .env
 app.listen(process.env.PORT, () => {
     console.log(`server launch on port ${process.env.PORT}`);
 });
